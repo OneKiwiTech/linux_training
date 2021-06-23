cb2482f841a84a616c6f598395020d49e42d86e3
Ngoc Phu Truong <phutruong@kienminh.net>
2021-06-23 16:40:09 +0700

    update

--- driver/app/config.h
+++ driver/app/config.h
@@ -1,8 +1,8 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define GPIO_USE_LIB_WIRING_PI
-// #define GPIO_USE_LIB_BCM35
+// #define GPIO_USE_LIB_WIRING_PI
+#define GPIO_USE_LIB_BCM35
 
 
 #endif /* SEGMENT_DISPLAY_H_ */
